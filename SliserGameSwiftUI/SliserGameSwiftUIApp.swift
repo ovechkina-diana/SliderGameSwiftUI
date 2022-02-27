a16c97323b7d4c1514cd5a86ffa173f89e1279c0
Diana Ovechkina <ovechkinadiana@yandex.ru>
2022-02-27 19:50:38 +0300

    Initial Commit

---
+++ SliserGameSwiftUI/SliserGameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliserGameSwiftUIApp.swift
+//  SliserGameSwiftUI
+//
+//  Created by Diana Ovechkina on 27.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliserGameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
